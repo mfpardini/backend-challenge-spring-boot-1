f24daa36af2802ec62bb3d81c31f92fa8a80f3f3
mfpardini <matheusfpardini@gmail.com>
2022-03-16 15:18:43 -0300

    new: especifc exception for resource not found

--- src/main/java/com/pardini/vuttr/api/services/ToolService.java
+++ src/main/java/com/pardini/vuttr/api/services/ToolService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pardini.vuttr.api.exceptions.ResourceNotFoundException;
 import com.pardini.vuttr.domain.dtos.ToolDto;
 import com.pardini.vuttr.domain.model.Tool;
 import com.pardini.vuttr.domain.repositories.ToolRepository;
@@ -15,40 +16,40 @@ public class ToolService {
 
 	@Autowired
 	private ToolRepository toolRepository;
-	
+
 	public List<Tool> getAll() {
 		return this.toolRepository.findAll();
 	}
-	
+
 	public Tool getById(String id) {
 		return this.getRegisterById(id);
 	}
-	
+
 	public List<Tool> getByTag(String tag) {
 		return this.toolRepository.findByTags(tag);
 	}
-	
+
 	public Tool update(String id, ToolDto objDto) {
 		Tool tool = this.getRegisterById(id);
 		BeanUtils.copyProperties(objDto, tool);
 		return this.toolRepository.save(tool);
 	}
-	
+
 	public Tool save(ToolDto objDto) {
 		Tool obj = new Tool();
 		BeanUtils.copyProperties(objDto, obj);
 		return this.toolRepository.save(obj);
 	}
-	
+
 	public void delete(String id) {
 		Tool obj = this.getRegisterById(id);
 		this.toolRepository.delete(obj);
 	}
-	
+
 	private Tool getRegisterById(String id) {
 		return this.toolRepository
-				.findById(id)
-				.orElseThrow(() -> new IllegalArgumentException("Tool does not exist."));
+			.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("Tool does not exist"));
 	}
-	
+
 }
