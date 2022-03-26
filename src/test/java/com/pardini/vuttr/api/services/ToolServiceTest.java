9c56690de102cf32e0c5838fcb3c2eeb64309313
mfpardini <matheusfpardini@gmail.com>
2022-03-26 10:53:02 -0300

    toolService tests for searchByTag and update

--- src/test/java/com/pardini/vuttr/api/services/ToolServiceTest.java
+++ src/test/java/com/pardini/vuttr/api/services/ToolServiceTest.java
@@ -1,6 +1,8 @@
 package com.pardini.vuttr.api.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -15,11 +17,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.pardini.vuttr.api.exceptions.ResourceNotFoundException;
+import com.pardini.vuttr.domain.dtos.ToolDto;
 import com.pardini.vuttr.domain.model.Tool;
 import com.pardini.vuttr.domain.repositories.ToolRepository;
 
@@ -39,9 +43,9 @@ public class ToolServiceTest {
 
 	@BeforeEach
 	public void setUp() {
-		tool1 = new Tool("a", "a", "a", "a", new ArrayList<>(Arrays.asList("a1", "a2")));
-		tool2 = new Tool("b", "b", "b", "b", new ArrayList<>(Arrays.asList("b1", "b2")));
-		tool3 = new Tool("c", "c", "c", "c", new ArrayList<>(Arrays.asList("c1", "c2")));
+		tool1 = new Tool("id1", "title1", "link1", "description1", new ArrayList<>(Arrays.asList("tag1", "tag2")));
+		tool2 = new Tool("id2", "title2", "link2", "description2", new ArrayList<>(Arrays.asList("tag2", "tag3")));
+		tool3 = new Tool("id3", "title3", "link3", "description3", new ArrayList<>(Arrays.asList("tag3", "tag1")));
 
 		toolList = new ArrayList<Tool>();
 		toolList.add(tool1);
@@ -52,6 +56,21 @@ public class ToolServiceTest {
 	public void tearDown() {
 		tool1 = tool2 = tool3 = null;
 		toolList = null;
+	}
+
+	/**
+	 * Perform assertions when trying to get resource that doesn't exists.
+	 */
+	private void testWhenShouldThrowResourceNotFoundException() {
+		when(mockRepository.findById("d")).thenReturn(Optional.ofNullable(null));
+
+		ResourceNotFoundException thrown = Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			toolService.getById("d");
+		}, "Resource not found exception was expected");
+
+		assertTrue(thrown.getMessage().contains("not found"));
+
+		verify(mockRepository, times(1)).findById("d");
 	}
 
 	@Test
@@ -67,26 +86,67 @@ public class ToolServiceTest {
 
 	@Test
 	public void whenSearchById_shoulReturnATool() {
-		when(mockRepository.findById("c")).thenReturn(Optional.of(tool3));
+		when(mockRepository.findById("id3")).thenReturn(Optional.of(tool3));
 
-		var returned = toolService.getById("c");
+		var returned = toolService.getById("id3");
 
 		assertThat(returned).isSameAs(tool3);
 
-		verify(mockRepository, times(1)).findById("c");
+		verify(mockRepository, times(1)).findById("id3");
 
 	}
 
 	@Test
 	public void whenSearchNonExistingId_shouldThrowResourceNotFoundException() {
-		when(mockRepository.findById("d")).thenReturn(Optional.ofNullable(null));
-
-		ResourceNotFoundException thrown = Assertions.assertThrows(ResourceNotFoundException.class, () -> {
-			toolService.getById("d");
-		}, "Resource not found exception was expected");
-
-		assertTrue(thrown.getMessage().contains("not found"));
-
-		verify(mockRepository, times(1)).findById("d");
+		testWhenShouldThrowResourceNotFoundException();
 	}
+
+	@Test
+	public void whenSearchByTag_shouldReturnAListOfMatchedTools() {
+		var expected = new ArrayList<Tool>(Arrays.asList(tool2, tool3));
+		when(mockRepository.findByTags("tag3")).thenReturn(expected);
+
+		var returned = toolService.getByTag("tag3");
+
+		assertThat(returned).isSameAs(expected);
+
+		verify(mockRepository, times(1)).findByTags("tag3");
+	}
+
+	@Test
+	public void whenUpdate_shouldReturnUpdatedTool() {
+		ToolDto toolDto = new ToolDto();
+		toolDto.setDescription("description4");
+		toolDto.setLink("link4");
+		toolDto.setTitle("title4");
+		toolDto.setTags(new ArrayList<String>(Arrays.asList("tag4", "tag1")));
+
+		when(mockRepository.findById("id3")).thenReturn(Optional.of(tool3));
+
+		Tool expected = new Tool("id3", "title4", "link4", "description4",
+				new ArrayList<String>(Arrays.asList("tag4", "tag1")));
+
+		when(mockRepository.save(Mockito.any(Tool.class))).thenReturn(expected);
+
+		var returned = toolService.update("id3", toolDto);
+
+		// Tool equals compare only by id
+		assertThat(returned).isEqualTo(expected);
+
+		assertAll("Should have the same properties", 
+			() -> assertEquals(expected.getTitle(), returned.getTitle()),
+			() -> assertEquals(expected.getLink(), returned.getLink()),
+			() -> assertEquals(expected.getDescription(), returned.getDescription()),
+			() -> assertEquals(expected.getTags(), returned.getTags())
+		);
+
+		verify(mockRepository, times(1)).findById("id3");
+		verify(mockRepository, times(1)).save(Mockito.any(Tool.class));
+	}
+	
+	@Test
+	public void whenUpdateNonExistingTool_shouldThrowResourceNotFoundException() {
+		testWhenShouldThrowResourceNotFoundException();
+	}
+
 }
